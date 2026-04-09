f078283eeb387402451e0f8753ac8a74cc0b5668
Tohirov Abdulloh <tohirovsarvar219@gmail.com>
2026-04-09 21:01:41 +0500

    Add ADMIN_APP_URL to config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-BOT_TOKEN    = "8796024744:AAHgqewn8vc4JieUw9Zl9Bx7BdSWmA6ZM5Q"
-ADMIN_ID     = 8321214528
-MINI_APP_URL = "https://family-burger-app.vercel.app"
-JSONBIN_ID   = "69d77d55aaba882197dd1746"
-JSONBIN_KEY  = "$2a$10$nRkSTdJDfYyxIy84qlZOEufkHuxqWdmACat3IUXajUiHIFf8Uo1Eu"
+BOT_TOKEN     = "8796024744:AAHgqewn8vc4JieUw9Zl9Bx7BdSWmA6ZM5Q"
+ADMIN_ID      = 8321214528
+MINI_APP_URL  = "https://family-burger-app.vercel.app"
+ADMIN_APP_URL = "https://family-burger-app.vercel.app/admin.html"
+JSONBIN_ID    = "69d77d55aaba882197dd1746"
+JSONBIN_KEY   = "$2a$10$nRkSTdJDfYyxIy84qlZOEufkHuxqWdmACat3IUXajUiHIFf8Uo1Eu"
