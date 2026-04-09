23f9c1df660fb6da644a455d61be90fe8ff4205c
Tohirov Abdulloh <tohirovsarvar219@gmail.com>
2026-04-09 19:29:01 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN    = "YOUR_BOT_TOKEN_HERE"
+ADMIN_ID     = 123456789
+MINI_APP_URL = "https://family-burger-app.vercel.app"
+JSONBIN_ID   = "69d77d55aaba882197dd1746"
+JSONBIN_KEY  = "$2a$10$nRkSTdJDfYyxIy84qlZOEufkHuxqWdmACat3IUXajUiHIFf8Uo1Eu"
